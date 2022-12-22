26bc79e81ffaf8a04b950d5d10a96f59b6e5c5b1
kylecollie <kcollie@gmail.com>
2022-12-22 13:27:25 -0500

    WIP: add exp form

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
 
 # Create your views here.
 
 @login_required(login_url='/authentication/login')
 def index(request):
+    categories = Category.objects.all()
     return render(request, 'expenses/index.html')
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'expenses/add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expense.html', context)
