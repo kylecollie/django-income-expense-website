a5d9837b23f7354071b1f5a13b10387279677148
kylecollie <kcollie@gmail.com>
2022-12-24 09:39:50 -0500

    Add Create Expense

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
@@ -13,13 +13,28 @@ def index(request):
 def add_expense(request):
     categories = Category.objects.all()
     context = {
-        'categories': categories
+        'categories': categories,
+        'values': request.POST
     }
-    return render(request, 'expenses/add_expense.html', context)
+    if request.method =='GET':
+        return render(request, 'expenses/add_expense.html', context)
+        
 
     if request.method == 'POST':
         amount = request.POST['amount']
+        description = request.POST['description']
+        date = request.POST['date']
+        category = request.POST['category']
 
         if not amount:
             messages.error(request, 'Amount is required')
             return render(request, 'expenses/add_expense.html', context)
+
+        if not description:
+            messages.error(request, 'Description is required')
+            return render(request, 'expenses/add_expense.html', context)
+        
+        Expense.objects.create(owner=request.user, amount=amount, date=date, category=category, description=description)
+        messages.success(request, 'Expense saved succesfully')
+
+        return redirect('expenses')
