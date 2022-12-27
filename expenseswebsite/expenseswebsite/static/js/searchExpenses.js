74d9c901c4940ded0c96302744b549786141aaf1
kylecollie <kcollie@gmail.com>
2022-12-27 08:20:39 -0500

    WIP: search

---
+++ expenseswebsite/expenseswebsite/static/js/searchExpenses.js
@@ -0,0 +1,8 @@
+searchField = document.querySelector('#searchField');
+searchField.addEventListener('keyup', (e) => {
+    const searchValue = e.target.value;
+
+    if (searchValue.length > 0) {
+        console.log('searchValue', searchValue);
+    }
+});
