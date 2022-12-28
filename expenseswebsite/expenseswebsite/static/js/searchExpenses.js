ffc8aa733fe6a25fa368ca3a08121d63fb48f0b6
kylecollie <kcollie@gmail.com>
2022-12-28 08:40:50 -0500

    Add search

--- expenseswebsite/expenseswebsite/static/js/searchExpenses.js
+++ expenseswebsite/expenseswebsite/static/js/searchExpenses.js
@@ -1,9 +1,17 @@
-searchField = document.querySelector('#searchField');
+const searchField = document.querySelector('#searchField');
+const tableOutput = document.querySelector('.table-output');
+const appTable = document.querySelector('.app-table');
+const tableBody = document.querySelector('.table-body');
+const paginationContainer = document.querySelector('.pagination-container');
+
+tableOutput.style.display = 'none';
+
 searchField.addEventListener('keyup', (e) => {
     const searchValue = e.target.value;
 
     if (searchValue.trim().length > 0) {
-        console.log('>>> searchValue: ', searchValue);
+        paginationContainer.style.display = 'none';
+        tableBody.innerHTML = '';
 
         fetch("/search-expenses", {
             body: JSON.stringify({ searchText: searchValue }),
@@ -11,7 +19,26 @@ searchField.addEventListener('keyup', (e) => {
         })
             .then((res) => res.json())
             .then((data) => {
-                console.log('>>> data: ', data);
+                appTable.style.display = 'none';
+                tableOutput.style.display = 'block';
+                if (data.length === 0) {
+                    tableOutput.innerHTML = 'No results found';
+                } else {
+                    data.forEach(item => {
+                        tableBody.innerHTML += `
+                        <tr>
+                        <td>${item.amount}</td>
+                        <td>${item.category}</td>
+                        <td>${item.description}</td>
+                        <td>${item.date}</td>
+                        </tr>
+                        `
+                    });
+                }
             });
-    }
+        } else {
+            tableOutput.style.display = 'none';
+            appTable.style.display = 'block';
+            paginationContainer.style.display = 'block';
+        }
 });
